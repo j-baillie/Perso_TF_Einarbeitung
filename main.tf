bab85beee6917d3bb3d42431dd2da0ecfb04a8bf
j-baillie <167808031+j-baillie@users.noreply.github.com>
2024-07-09 12:11:24 +0200

    housekeeping

--- main.tf
+++ main.tf
@@ -30,40 +30,8 @@ locals {
   # list of declaration of variables that we can re-use elsewhere for ease.
   # locals are variables that contain just one element
 }
-/*
-data aws_iam_policy_document "ec2_assume_role" {
-  statement {
-    actions = ["sts:AssumeRole"]
-    principals {
-      type        = "Service"
-      identifiers = ["ec2.amazonaws.com"]
-    }
-  }
-}
-
-data aws_iam_policy_document "s3_read_access" {
-  statement {
-    actions   = ["s3:Get*", "s3:List*"]
-    resources = ["arn:aws:s3:::*"]
-  }
-  #using these data resources, we are able to GENERATE policy config documents with the actions/permissions on the resources we define
-}
-*/
-
-/*
-A Local. is only accessible within the local module vs a Terraform variable., which can be scoped globally.
-Another thing to note is that a local in Terraform doesn’t change its value once assigned. A variable value can be manipulated via expressions
-https://spacelift.io/blog/terraform-locals
-*/
 
 # variable in this case means an input variable - we are inputting a variable that contains many elements that can be referenced globally.
-
-/*
-variable "public_subnet_ids" {
-  description = "List of the available public subnet ID's."
-  type = list(string)
-}
-*/
 
 data "terraform_remote_state" "AWSAccountSetup" {
   backend   = "s3"
@@ -86,10 +54,6 @@ data "terraform_remote_state" "AWSNetworkState" {
     dynamodb_table = var.AWSNetworkState
   }
 }
-
-/*output "yyyy" {
-  value = data.terraform_remote_state.AWSNetworkState.outputs
-}*/
 
 resource "aws_security_group" "allowed_traffic" {
   name        = "${local.kurz}-allowed_traffic_in"
@@ -142,7 +106,8 @@ resource "aws_instance" "AutoUbuntus" {
   associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.allowed_traffic.id]
   iam_instance_profile        = aws_iam_instance_profile.AutoUbuntuProfile.name
-  key_name = module.awskeydeploy.jonpubkeyname ##you can call specific arguments out of modules that have been loaded
+  key_name = module.awskeydeploy.jonpubkeyname
+  ##you can call specific arguments out of modules that have been loaded
   user_data                   = "${file("./install_apache.sh")}"
 
   tags = {
@@ -203,22 +168,10 @@ resource "aws_instance" "CountUbuntus" {
 
 module "awskeydeploy" {
   # here i am loading the module.
-  # when calling, i want to overwrite the VARIABLE of the MODULE with this data
+  # when calling, i want to overwrite the VARIABLE of the MODULE with this data - NOT the ARGUMENT of the RESOURCE invocation
   source        = "./aws_key_pair"
   jonpubkeypath = file("${path.cwd}/id_ed25519.pub")
 }
-
-/*
-#print into the console the public ip of statically created instance
-output "Z_JonManPubIP" {
-  value = aws_instance.JonUbuntuEc2.public_ip
-}
-
-#print into the console the public ips of the servers created with count meta
-output "Z_JonCountPubIP" {
-  value = { for k, instance in aws_instance.CountUbuntus : k => instance.public_ip } # for every instance, pull the information public_ip to k. print k
-}
-*/
 
 module "first_bucket" {
   source        = "./s3bucket"
@@ -245,7 +198,7 @@ module "iam_policies_init" {
 }
 
 resource "aws_iam_role" "AutoUbuntusRole" {
-  name               = "bjo-ubuntusrole"
+  name = "bjo-ubuntusrole"
   #assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
   assume_role_policy = module.iam_policies_init.ec2_assume_role.json
   #policy is defined elsewhere. Here we are pulling the json policy information created in the module over
@@ -253,7 +206,7 @@ resource "aws_iam_role" "AutoUbuntusRole" {
 
 resource "aws_iam_role_policy" "join_policy" {
   name   = "join_policy"
-  role   = aws_iam_role.AutoUbuntusRole.name
+  role = aws_iam_role.AutoUbuntusRole.name
   #policy = data.aws_iam_policy_document.s3_read_access.json
   policy = module.iam_policies_init.s3_read_acess.json
   #policy is defined elsewhere. Here we are pulling the json policy information created in the module over
@@ -264,24 +217,3 @@ resource "aws_iam_instance_profile" "AutoUbuntuProfile" {
   role = aws_iam_role.AutoUbuntusRole.name
   #
 }
-
-/*
-resource "aws_iam_role" "AutoUbuntusRole" {
-  name               = "${local.kurzklein}-ubuntusrole"
-  assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
-  #policy is defined elsewhere in this file. Here we are just pulling it over for its implementation to the role
-}
-
-resource "aws_iam_role_policy" "join_policy" {
-  name   = "join_policy"
-  role   = aws_iam_role.AutoUbuntusRole.name
-  policy = data.aws_iam_policy_document.s3_read_access.json
-  #policy is defined elsewhere in this file. Here we are just pulling it over for its implementation to the role
-}
-
-resource "aws_iam_instance_profile" "AutoUbuntuProfile" {
-  name = "${local.kurzklein}-ec2InstanceProfile"
-  role = aws_iam_role.AutoUbuntusRole.name
-  #
-}
-*/
